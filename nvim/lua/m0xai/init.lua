f2fc05f2400824c2958038b9078cd9dd5c245f5e
Kerem Zopcuk <keremzopcuk@gmail.com>
2022-12-19 18:32:33 +0100

    first working commit of new terminal life.

---
+++ nvim/lua/m0xai/init.lua
@@ -0,0 +1,3 @@
+require("m0xai.remap")
+require("m0xai.packer")
+require("m0xai.set")
---
+++ nvim/lua/m0xai/set.lua
@@ -0,0 +1,25 @@
+vim.opt.nu = true
+vim.opt.rnu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 6
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+
+vim.g.mapleader = " "
