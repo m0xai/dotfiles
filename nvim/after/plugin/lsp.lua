2878672393698c65c64418ca22c622b2f0f3432b
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-01-08 20:15:36 +0100

    reorder key mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -62,16 +62,16 @@ lsp.on_attach(function(client, bufnr)
 --  end
 
   vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "<leader>gcf", vim.lsp.buf.format, opts)
-  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
   vim.keymap.set("n", "<leader>gn", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "<leader>grf", vim.lsp.buf.format, opts)
+  vim.keymap.set("n", "<leader>grn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
 
   vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-  vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>grn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
