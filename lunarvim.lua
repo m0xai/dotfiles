498b1a478e2884dc84e48753a8e9588dbd2bb4c4
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-04-12 11:58:01 +0200

    Update lunarvim.lua

--- lunarvim.lua
+++ lunarvim.lua
@@ -1,7 +1,24 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
 lvim.keys.insert_mode["jj"] = "<ESC>"
 lvim.keys.insert_mode["jk"] = "<ESC>"
+lvim.keys.normal_mode["gt"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["gT"] = ":BufferLineCyclePrev<CR>"
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+
+vim.opt.wrap = true
 
 lvim.transparent_window = true
+lvim.format_on_save.enabled = true
 
 lvim.plugins = {
   {
@@ -11,8 +28,26 @@ lvim.plugins = {
       vim.cmd("call vimtex#init()")
     end,
   },
-  {"catppuccin/nvim",
-      init = function()
-    end
+  {
+    'mhartington/oceanic-next'
+  },
+  {
+    "lepture/vim-jinja"
   }
+}
+
+lvim.colorscheme = "OceanicNext"
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { name = "black" },
+}
+
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    name = "shellcheck",
+    args = { "--severity", "warning" },
+  },
 }
